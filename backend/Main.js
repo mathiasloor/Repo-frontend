5688ef10521fd25b175cacc9401b36288bd56e42
byronloor <loorsb@gmail.com>
2020-10-19 19:22:44 -0500

    Versión 3
    Incluye video 3.  Está pendiente crear la DB

---
+++ backend/Main.js
@@ -0,0 +1,52 @@
+const express       = require('express');
+const app           = express();
+const path          = require('path');
+const mysql         = require('mysql');
+const session       = require('express-session');
+const MySQLStore    = require('express-mysql-session')(session) ;
+const Router        = require('./Router'); 
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
+
+// Database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'myapp'
+});
+
+db.connect(function(err) {
+    if (err) {
+        console.log('DB error');
+        throw err;
+        return false;
+    }    
+});
+
+const sessionStore = new MySQLStore({
+    expiration: (1825 * 86400 * 1000),
+    endConnectionOnClose: false
+}, db);
+
+app.use(session({
+    key: '4289thgub4390ghejgbsdifhsd',
+    secret: 'mg3g48hddnxnjnALKFJjdhfadfj' ,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
+    cokie: {
+        maxAge: (1825 * 86400 * 1000),
+        httpOnly: false
+    }
+}));
+
+new Router(app,db);
+
+app.get('/',function(req, res) {
+    res.sendFile(path.join(:dirname, 'build', 'index.html'));
+});
+
+app.listen(3000);
+
